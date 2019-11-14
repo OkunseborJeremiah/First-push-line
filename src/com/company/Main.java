90bce1f073482e974d870208bae655567591b04c
OkuseborJeremiah <felixokuns2007@gmail.com>
2019-11-14 06:16:47 -0800

    First Inital Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+ System.out.println("Okunsebor");
+    System.out.println("Isimenmen");
+    System.out.println("Jeremiah");}
+}
